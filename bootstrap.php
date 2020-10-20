1dfd35f468a1d41f2b2a623d9168fd09c0b1ef57
Mahdi Aslami <mahdyaslami@gmail.com>
2020-10-20 22:52:28 +0330

    Install helpers and structure of pluginable.

---
+++ bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+if (! function_exists('get_plugins')) {
+    function get_plugins()
+    {
+        # code...
+    }
+}
+
+if (! function_exists('save_plugin')) {
+    function save_plugin($pluginname)
+    {
+        # code...
+    }
+}
+
+if (! function_exists('has_plugin')) {
+    function has_any_plugin($pluginname)
+    {
+        # code...
+    }
+}
+
+if (! function_exists('need_to_check_workspace')) {
+    function has_any_plugin()
+    {
+        # code...
+    }
+}
+
+if (! function_exists('override_workspace')) {
+    function override_workspace($pathToWorkspace)
+    {
+        # code...
+    }
+}
